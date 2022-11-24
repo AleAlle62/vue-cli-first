aa6ca086fc0365449350b3a66a92ed8bebc21721
AleAlle62 <basketallegrini@gmail.com>
2022-11-24 12:03:53 +0100

    end2

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <variabileTitolo></variabileTitolo>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import variabileTitolo from '@/components/TitoloPagina.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    components : {
+        variabileTitolo,
+    },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/TitoloPagina.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1 class="titolo">hello word</h1>
+</template>
+
+<script>
+export default {
+    name : 'variabileTitolo'
+}
+</script>
+
+<style>
+    .titolo {
+        color: salmon;
+    }
+</style>
